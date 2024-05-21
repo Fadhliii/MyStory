cad1af15332c694d9200689667c404d6f813ade6
Fadhli M <fadhli2316@gmail.com>
2024-05-22 03:46:48 +0700

    3rd commt || deleting unncessary comment on ApiService

--- app/src/main/java/com/example/mystory04/ApiService.kt
+++ app/src/main/java/com/example/mystory04/ApiService.kt
@@ -57,29 +57,4 @@ interface ApiService {
     suspend fun getStory(
             @Header("Authorization") authorization: String
     ): DetailStoryResponse
-
-//    @Headers("Authorization: token <Personal Access Token>")
-//    @GET("api/users")
-//    fun getListUsers(@Query("page") page: String): Call<ResponseUser>
-//
-//    // Mendapatkan daftar pengguna berdasarkan id dengan path.
-//    @GET("api/users/{id}")
-//    fun getUser(@Path("id") id: String): Call<ResponseUser>
-//
-//    // Mengirim data pengguna menggunakan x-www-form-urlencoded.
-//    @FormUrlEncoded
-//    @POST("api/users")
-//    fun createUser(
-//            @Field("name") name: String,
-//            @Field("job") job: String
-//    ): Call<ResponseUser>
-//
-//    // Mengunggah berkas menggunakan Multipart
-//    @Multipart
-//    @PUT("api/uploadfile")
-//    fun updateUser(
-//            @Part("file") file: MultipartBody.Part,
-//            @PartMap data: Map<String, RequestBody>
-//    ): Call<ResponseUser>
-
 }
